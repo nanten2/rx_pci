55d66b9d0241cacb7314d7d0771cc19313465bcf
KazukiShiotani <shiotani@a.phys.nagoya-u.ac.jp>
2018-07-10 20:17:20 +0000

    update

--- scripts/nasco_sisbb_controller.py
+++ scripts/nasco_sisbb_controller.py
@@ -1,11 +1,11 @@
 import rospy
 import time
-from rx_pci_single_ros.msg import nasco_sisbb_controller_msg
+from rx_pci_single_ros.msg import nasco_sisbb_sub_msg
 
 node_name = 'sisbb_controller'
 rospy.init_node(node_name)
 
-pub = rospy.Publisher('sisbb_controller', nasco_sisbb_controller_msg, queue_size=1)
+pub = rospy.Publisher('nasco_sisbb_command', nasco_sisbb_sub_msg, queue_size=1)
 
 def nasco_sisbb_set_voltage(ch, voltage, interval):
     """

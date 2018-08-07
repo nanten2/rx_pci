66072f53e4be0fe542cc0bf65c67704d7bdc32ac
keisukesakasai <sakasai@a.phys.nagoya-u.ac.jp>
2018-08-08 00:51:12 +0900

    add sg-ros ver.1

--- scripts/e8250d_signal_generator.py
+++ scripts/e8250d_signal_generator.py
@@ -1,10 +1,8 @@
 #! /usr/bin/env python3
 
 import rospy
-import std_msgs
 from std_msgs.msg import Float64
 
-import os
 import sys
 import time
 import threading
@@ -16,10 +14,17 @@ class e8257d_controller(object):
 
     def __init__(self):
         self.pinterval = 0.1
-        self.sg = E8257D.e8257d()
+        self.host = rospy.get_param('~host')
+        self.port = rospy.get_param('~port')
+        self.sg = E8257D.e8257d(self.host, self.port)
+        self.pub_freq = rospy.Publisher('lo_1st_freq', Float64, quere_size=1)
+        self.pub_power = rospy.Publisher('lo_1st_power', Float64, quere_size=1)
+        self.pub_onoff = rospy.Publisher('lo_1st_onoff', Float64, quere_size=1)
+        self.sub_freq.rospy.Subscriber('lo_1st_freq.cmd', lo_1st_freq_msg, _set_freq)
+        self.sub_power.rospy.Subscriber('lo_1st_power.cmd', lo_1st_power_msg, _set_power)
+        self.rospy.Subscriber('lo_1st_onoff.cmd', lo_1st_onoff_msg, _set_onoff)
 
     def _set_freq(self, q):
-        pub = rospy.Publisher('lo_1st_freq', Float64, quere_size=1)
         target = q.lo_1st_freq
         self.sg.set_freq(target, 'GHz')
 
@@ -27,34 +32,32 @@ class e8257d_controller(object):
             current = self.sg.query_freq()
 
             if target != cuurent:
-                pub.publish(current)
+                self.pub_freq.publish(current)
                 time.sleep(self.pinterval)
                 pass
 
             elif target == cuurent:
-                pub.publish(curent)
+                self.pub_freq.publish(curent)
                 time.sleep(self.pinterval)
                 break
 
     def _set_power(self, q):
-        pub = rospy.Publisher('lo_1st_power', Float64, quere_size=1)
         self.sg.set_power(target)
 
         while True:
             current = self.sg.query_power()
 
             if target != cuurent:
-                pub.publish(current)
+                self.pub_power.publish(current)
                 time.sleep(self.pinterval)
                 pass
 
             elif target == cuurent:
-                pub.publish(cuurent)
+                self.pub_power.publish(cuurent)
                 time.sleep(self.pinterval)
                 break
 
     def _set_onoff(self, q):
-        pub = rospy.Publisher('lo_1st_onoff', Float64, quere_size=1)
         target = q.lo_1st_onoff
         self.sg.set_onoff(target)
 
@@ -63,12 +66,12 @@ class e8257d_controller(object):
             msg.lo_1st_onoff_msg = current
 
             if target != cuurent:
-                pub.publish(current)
+                self.pub_onoff.publish(current)
                 time.sleep(self.pinterval)
                 pass
 
             elif target == cuurent:
-                pub.publish(current)
+                self.pub_onoff.publish(current)
                 time.sleep(self.pinterval)
                 break
 
@@ -77,7 +80,4 @@ if __name__ == '__main__':
     sg_ctrl = e8257d_controller()
     sg_ctrl.start_thread_ROS()
     print('[e8257d_signal_generetor] : START SUBSCRIBER ... ')
-    sub_freq = rospy.Subscriber('lo_1st_freq.cmd', lo_1st_freq_msg, _set_freq)
-    sub_freq = rospy.Subscriber('lo_1st_power.cmd', lo_1st_power_msg, _set_power)
-    sub_freq = rospy.Subscriber('lo_1st_onoff.cmd', lo_1st_onoff_msg, _set_onoff)
     rospy.spin()
